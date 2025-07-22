8588189a9c7d35dd11692ea3689e4cca3c45d7ae
Mariam Mohamed Ossama <mariamosama772@gmail.com>
2025-07-23 00:40:35 +0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Footer } from "./components/footer/footer";
+import { Home } from "./components/home/home";
+import { About } from "./components/about/about";
+import { Portfolio } from "./components/portfolio/portfolio";
+import { Contact } from "./components/contact/contact";
+import { ContactInfo } from "./components/contact-info/contact-info";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Home, About, Portfolio, Contact, ContactInfo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
