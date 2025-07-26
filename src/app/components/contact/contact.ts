6f0da933f71befde6eb09af4fce1010e810c6757
Mariam Mohamed Ossama <mariamosama772@gmail.com>
2025-07-26 21:55:50 +0300

    contact label displayed on ininput

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
-
+name:string = '';
+age?:number;
+email:string = '';
+password:string = '';
 }
